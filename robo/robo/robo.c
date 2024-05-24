f8a01233ec2dd6f9fad32810cae95386d02a6c1e
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2024-05-24 19:15:31 -0300

    next0

--- robo/robo/robo.c
+++ robo/robo/robo.c
@@ -3,6 +3,7 @@
 	#include <Windows.h>
 	#define ROWS 8
 	#define COLS 8
+	int x, y;
 	int dx, dy;
 	char matrix[ROWS][COLS] = {
 			{'-', '-', '-', '-', '-', '-', '-', '-'},
@@ -28,7 +29,6 @@
 	}
 
 	int robotEstation() {
-		int x, y;
 		printf("digite onde voce quer a estacão no eixo horizontal digite apenas de 0 a 7: ");
 		scanf_s("%d", &x);
 		printf("digite onde voce quer a estacão no eixo vertical digite apenas de 0 a 7: ");
@@ -70,12 +70,36 @@
 		}
 	}
 	void cleanDirty() {
+		char tempMatrix[ROWS][COLS];
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLS; j++) {
+				tempMatrix[i][j] = matrix[i][j];
+			}
+		}
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLS; j++) {
+				if (tempMatrix[i][j] == '#') {
+					matrix[i][j] = ' ';
+					matrix[i - 1][j] = ' ';
+					if (i + 1 < ROWS && tempMatrix[i + 1][j] == '#')
+						matrix[i + 1][j] = ' ';
 
+					if (j - 1 >= 0 && tempMatrix[i][j - 1] == '#')
+						matrix[i][j - 1] = ' ';
+					if (j + 1 < COLS && tempMatrix[i][j + 1] == '#')
+						matrix[i][j + 1] = ' ';
+				}
+			}
+			printMatrix();
+		}
+		Sleep(1000);
 	}
+
 
 	int main() {
 		printMatrix();
 		robotEstation();
 		dirtyMatrix();
+		cleanDirty();
 		return 0;
 	}
